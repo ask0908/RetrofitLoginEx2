07594ba6ac879e71f79205702d6f6d2c0c362221
intae-98 <askdfjaslk@naver.com>
2020-01-30 12:45:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/retrofitloginex2/ApiInterface.java
@@ -0,0 +1,21 @@
+package com.example.retrofitloginex2;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("register.php")
+    Call<User> performRegistration(
+            @Query("name") String name,
+            @Query("user_name") String UserName,
+            @Query("user_password") String UserPassword);
+
+    @GET("login.php")
+    Call<User> performUserLogin(
+            @Query("user_name") String UserName,
+            @Query("user_password") String UserPassword
+    );
+
+}
---
+++ app/src/main/java/com/example/retrofitloginex2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.retrofitloginex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnLoginFormActivityListener {
+
+    public static PrefConfig prefConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefConfig = new PrefConfig(this);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            if (prefConfig.readLoginStatus()) {
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new WelcomeFragment()).commit();
+            } else {
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new LoginFragment()).commit();
+            } // 여기까지 코딩했고 집 가서 핸드폰 연결하고 어떻게 되는지 확인하기(영상 42:47~부터 보면 됨)
+
+        }
+    }
+
+    @Override
+    public void performRegister()
+    {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                                               new RegistrationFragment())
+                .addToBackStack(null).commit();
+    }
+
+    @Override
+    public void performLogin(String name)
+    {
+
+    }
+}
